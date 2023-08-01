3f9d67d5c00d3741575641d3c82f704cdf2a1b61
olivia leese <116760201+ol287@users.noreply.github.com>
2023-08-01 19:47:03 +0100

    Add files via upload

---
+++ webbroser.py
@@ -0,0 +1,65 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('https://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+        
+        self.statusBar().showMessage("Welcome to Olivia's Fancy Browser!")
+        
+        self.setStyleSheet(
+                "QToolBar { background-color: #FF69B4; color: white; }"  
+                "QToolBar QToolButton { background-color: #FFB6C1; }" 
+                "QLineEdit { background-color: #FFC0CB; color: black; }"  
+                "QMainWindow { background-color: #FFC0CB; }"  
+            )
+
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('https://www.instagram.com/oliviaxleese/'))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Olivia Leese Browser')
+window = MainWindow()
+app.exec_()
